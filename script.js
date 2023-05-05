0973feff98b2fcaa40bcc68014196f8b223acd2b
Thanushkar Sivakumar <thanushkarsivakumar@gmail.com>
2023-05-05 22:06:53 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function input(e) {
+    var tbInput = document.getElementById("tbInput");
+    tbInput.value = tbInput.value + e.value;
+}
+
+function del() {
+    var tbInput = document.getElementById("tbInput");
+    tbInput.value = tbInput.value.substr(0, tbInput.value.length - 1);
+}
+function display(value) {
+    document.getElementById("tbInput").value += value;
+}
+function calculate() {
+    var p = document.getElementById("tbInput").value;
+    var q = eval(p);
+    document.getElementById("tbInput").value = q;
+}
