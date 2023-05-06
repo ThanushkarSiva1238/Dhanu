270699cf834185bc6eb75c50117aeaa13d89ca65
Thanushkar Sivakumar <121393822+ThanushkarSiva1238@users.noreply.github.com>
2023-05-06 15:51:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-
-
 function del() {
     var tbInput = document.getElementById("tbInput");
     tbInput.value = tbInput.value.substr(0, tbInput.value.length - 1);
 }
+
 function display(value) {
     document.getElementById("tbInput").value += value;
 }
+
 function calculate() {
     var p = document.getElementById("tbInput").value;
     var q = eval(p);
